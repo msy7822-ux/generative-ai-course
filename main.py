9bde4be8f802c58b0022e9cad227a816aafbc5a8
msy7822-ux <msy780022@gmail.com>
2024-01-06 16:31:12 +0900

    PDFの内容を解答してくれるAssistant APIを実装

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def encode_image(image_path):
 
 
 # Fileのアップロード
-uploaded_file = openai.files.create(
-    file=open("attention_is_all_you_need.pdf", "rb"),
+file = openai.files.create(
+    file=open("./attention_is_all_you_need.pdf", "rb"),
     purpose="assistants",
 )
 
+print(file)
 import time
 
 
@@ -35,19 +36,13 @@ def wait_on_run(run, thread):
     return run
 
 
-# assistant = openai.beta.assistants.create(
-#     name="Prompt Engineer Bot",
-#     description="Transformerモデルについて詳しいアシスタントです。",
-#     model="gpt-4-1106-preview",
-#     instructions="あなたは、Transformerモデルについて詳しいアシスタントです。Attentionに関するPDFの教材を参考にして、回答してください。",
-#     tools=[{"type": "retrieval"}, {"type": "code_interpreter"}],
-#     file_ids=[file.id],
-# )
-
 assistant = openai.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Answer questions briefly, in a sentence or less.",
+    name="Prompt Engineer Bot",
+    description="Transformerモデルについて詳しいアシスタントです。",
     model="gpt-4-1106-preview",
+    instructions="あなたは、Transformerモデルについて詳しいアシスタントです。Attentionに関するPDFの教材を参考にして、回答してください。",
+    tools=[{"type": "retrieval"}, {"type": "code_interpreter"}],
+    file_ids=[file.id],
 )
 
 import json
@@ -60,18 +55,11 @@ def show_json(obj):
 thread = openai.beta.threads.create()
 thread_id = thread.id
 
-# openai.beta.threads.messages.create(
-#     thread_id=thread_id,
-#     role="user",
-#     content="PDFのタイトルを教えてください。",
-# )
-
 message = openai.beta.threads.messages.create(
-    thread_id=thread.id,
+    thread_id=thread_id,
     role="user",
-    content="I need to solve the equation `3x + 11 = 14`. Can you help me?",
+    content="Transformerモデルの基本的な構造を全て日本語で教えてください。",
 )
-# show_json(message)
 
 run = openai.beta.threads.runs.create(
     thread_id=thread_id,
@@ -85,11 +73,3 @@ messages = openai.beta.threads.messages.list(
 
 for message in messages.data:
     print(message.content[0].text.value)
-
-# run_retrieve = openai.beta.threads.runs.retrieve(
-#     thread_id=thread_id,
-#     run_id=run.id,
-# )
-
-
-# messages = openai.beta.threads.messages.list(thread_id=thread_id)
